57ca6f0a63665f70de20aa980c6bed11d72a862f
Raul Makendengue Rodriguez <rmakende@c2r8s6.42madrid.com>
2024-10-06 18:48:12 +0200

    Update

--- pereza.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-make fclean
-make all
-make clean
-sleep 1
-./push_swap 1 2 3 4 5 6 7 8 9
--- subir.sh
+++
@@ -1,7 +0,0 @@
-./bin/bash
-git add Libft
-git add moves
-git add printf *.c *.h
-git add Makefile
-git commit -m "Update para vagos"
-git push
